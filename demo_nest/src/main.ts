022604187e409f8d75a5d9e1230243e445e0d459
Noel Vaca <noel.vm.2501@gmail.com>
2025-09-25 20:09:04 -0400

    chore: prefijo y versionamiento de la api

--- demo_nest/src/main.ts
+++ demo_nest/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.setGlobalPrefix('api');
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
+  app.enableCors();
 
   await app.listen(process.env.PORT ?? 0);
   console.log(`App corriendo en ${await app.getUrl()}`);
